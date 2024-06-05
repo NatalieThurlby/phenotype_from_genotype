f649c8f83bffaa70f6eefb48ea226ac1ea5bf363
Natalie Zelenka <n.zelenka@ucl.ac.uk>
2024-06-05 09:36:16 +0100

    trying a different version of IRkernel

--- install.R
+++ install.R
@@ -1,5 +1,5 @@
 # Install specific version of IRkernel that works with our specific version of R
-package = "https://cran.r-project.org/package=IRkernel&version=1.0.2"
+package = "https://cran.r-project.org/package=IRkernel&version=1.1.1"
 install.packages(pkgs = package)
 IRkernel::installspec()
 
