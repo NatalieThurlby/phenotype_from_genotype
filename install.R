d2a74538bd8c01343d5fcc147374fa97af778501
Natalie Zelenka <n.zelenka@ucl.ac.uk>
2024-06-05 09:22:09 +0100

    Specifying version of IRkernel

--- install.R
+++ install.R
@@ -1,2 +1,5 @@
-install.packages('IRkernel')
+# Install specific version of IRkernel that works with our specific version of R
+package = "https://cran.r-project.org/package=IRkernel&version=1.0.2"
+install.packages(pkgs = package)
 IRkernel::installspec()
+
