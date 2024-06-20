9bfb5f7458333c0a4f1a18b0ca631cec7361ed41
Natalie Zelenka <n.zelenka@ucl.ac.uk>
2024-06-20 21:39:24 +0100

    Investigating problem package - glue?

--- install.R
+++ install.R
@@ -1,5 +1,7 @@
 # Install specific version of IRkernel that works with our specific version of R
 package = "https://cran.r-project.org/package=IRkernel&version=1.1.1"
+install.packages('glue')
+install.packages('pillar')
 install.packages('repr')
 install.packages('evaluate')
 install.packages('IRdisplay')
