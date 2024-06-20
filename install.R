44d1053f3c41de1700e68a902e129fc30ec8780b
Natalie Zelenka <n.zelenka@ucl.ac.uk>
2024-06-20 21:26:19 +0100

    Investigating problem package

--- install.R
+++ install.R
@@ -1,5 +1,14 @@
 # Install specific version of IRkernel that works with our specific version of R
 package = "https://cran.r-project.org/package=IRkernel&version=1.1.1"
+install.packages('repr')
+install.packages('evaluate')
+install.packages('IRdisplay')
+install.packages('pbdZMQ')
+install.packages('crayon')
+install.packages('jsonlite')
+install.packages('uuid')
+install.packages('digest')
+
 utils::install.packages(pkgs = package, repos = NULL)
 IRkernel::installspec()
 
