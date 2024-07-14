6f4758974198ccd6a0e11c21597449b1e4be85a4
Natalie Zelenka <n.zelenka@ucl.ac.uk>
2024-07-14 13:42:57 +0100

    reverting to earlier version of install.R now website builds

--- install.R
+++ install.R
@@ -1,19 +1,20 @@
 # Install specific version of IRkernel that works with our specific version of R
-IRkernel = "https://cran.r-project.org/package=IRkernel&version=1.1.1"
-glue = "https://cran.r-project.org/package=glue&version=1.2.0"
+install.packages('IRkernel')
+# IRkernel = "https://cran.r-project.org/package=IRkernel&version=1.1.1"
+# glue = "https://cran.r-project.org/package=glue&version=1.2.0"
 
-utils::install.packages(pkgs = glue, repos = NULL)
+# utils::install.packages(pkgs = glue, repos = NULL)
 
-install.packages('pillar')
-install.packages('repr')
-install.packages('evaluate')
-install.packages('IRdisplay')
-install.packages('pbdZMQ')
-install.packages('crayon')
-install.packages('jsonlite')
-install.packages('uuid')
-install.packages('digest')
+# install.packages('pillar')
+# install.packages('repr')
+# install.packages('evaluate')
+# install.packages('IRdisplay')
+# install.packages('pbdZMQ')
+# install.packages('crayon')
+# install.packages('jsonlite')
+# install.packages('uuid')
+# install.packages('digest')
 
-utils::install.packages(pkgs = IRkernel, repos = NULL)
+# utils::install.packages(pkgs = IRkernel, repos = NULL)
 IRkernel::installspec()
 
